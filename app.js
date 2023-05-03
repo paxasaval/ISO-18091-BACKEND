cc8a2fa61b49a7354a698a42c36d88d129c24635
paxasaval <paxasaval1003@gmail.com>
2023-05-02 19:17:26 -0500

    lint:fix + cors

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error conecting to MongoDB:', error.message)
   })
 
-app.use(cors())
+app.use(cors({
+  origin:'http://localhost:4200'
+}))
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
